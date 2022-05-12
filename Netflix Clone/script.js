b3805f57e95171a7a9bfd6618e01447dabfb3574
Daniel-Oyebode <Dannyoyebode@gmail.com>
2022-05-12 21:04:11 +0100

    Daniel's Project
    Daniel added his own part of the project

---
+++ Netflix Clone/script.js
@@ -0,0 +1,71 @@
+// Question 1
+const answer1 = document.getElementById("answer1")
+const questionbtn1 = document.getElementById("question1")
+
+questionbtn1.onclick = function () {
+    if (answer1.style.display !== "block") {
+        answer1.style.display = "block"
+    } else {
+        answer1.style.display = "none"
+    }
+}
+
+// Question 2
+const answer2 = document.getElementById("answer2")
+const questionbtn2 = document.getElementById("question2")
+
+questionbtn2.onclick = function () {
+    if (answer2.style.display !== "block") {
+        answer2.style.display = "block"
+    } else {
+        answer2.style.display = "none"
+    }
+}
+
+// Question 3
+const answer3 = document.getElementById("answer3")
+const questionbtn3 = document.getElementById("question3")
+
+questionbtn3.onclick = function () {
+    if (answer3.style.display !== "block") {
+        answer3.style.display = "block"
+    } else {
+        answer3.style.display = "none"
+    }
+}
+
+// Question 4
+const answer4 = document.getElementById("answer4")
+const questionbtn4 = document.getElementById("question4")
+
+questionbtn4.onclick = function () {
+    if (answer4.style.display !== "block") {
+        answer4.style.display = "block"
+    } else {
+        answer4.style.display = "none"
+    }
+}
+
+// Question 5
+const answer5 = document.getElementById("answer5")
+const questionbtn5 = document.getElementById("question5")
+
+questionbtn5.onclick = function () {
+    if (answer5.style.display !== "block") {
+        answer5.style.display = "block"
+    } else {
+        answer5.style.display = "none"
+    }
+}
+
+// Question 6
+const answer6 = document.getElementById("answer6")
+const questionbtn6 = document.getElementById("question6")
+
+questionbtn6.onclick = function () {
+    if (answer6.style.display !== "block") {
+        answer6.style.display = "block"
+    } else {
+        answer6.style.display = "none"
+    }
+}
